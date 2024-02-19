fb6fee808fc7cab6d5723072f2f72f19051f281b
ivancotacte <cotactearmenion@gmail.com>
2024-02-19 23:15:08 +0800

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
-require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+require('ejs');
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`);
 });
