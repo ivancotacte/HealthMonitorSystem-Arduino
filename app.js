fb1a4d0ac53230ecfa7a4f76a4d94c39fc02f15c
ivancotacte <cotactearmenion@gmail.com>
2024-03-08 16:34:06 +0800

    Add register route to app.js

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.get('/login', (req, res) => {
     res.render('login', { title: 'Login' });
 });
 
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Login" });
+});
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });
